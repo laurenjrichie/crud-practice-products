f251e2639f88fad4530828583700d0d894d832bd
laurenjrichie <laurenjrichie@gmail.com>
2014-12-08 11:08:50 -0800

    users can update a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,20 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.save
+      redirect_to products_path, notice: "Product was updated successfully"
+    else
+      render :edit
+    end
+  end
+
   private
   def product_params
     params.require(:product).permit(
