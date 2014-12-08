95ce0e8cfe282013ae3c45f24ff478820241c71d
laurenjrichie <laurenjrichie@gmail.com>
2014-12-08 11:18:51 -0800

    users can delete a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -34,6 +34,12 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, notice: "Product was deleted"
+  end
+
   private
   def product_params
     params.require(:product).permit(
