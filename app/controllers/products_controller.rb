0ce98acc5aee2a57207dd07960d98fe1b4e7d3b0
laurenjrichie <laurenjrichie@gmail.com>
2014-12-08 10:23:44 -0800

    users can create a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,29 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product), notice: "Product was successfully created"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :price,
+    )
+  end
 end
